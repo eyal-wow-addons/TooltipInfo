5d6feb53946872bc2e29064a5a993ef22f7fa2ab
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-02-27 23:35:24 +0200

    Fix a bug where class name can be nil

--- Scripts/Class.lua
+++ Scripts/Class.lua
@@ -13,16 +13,19 @@ TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Unit, function(tool
     if tooltip ~= GameTooltip then return end
 
     local _, unit = tooltip:GetUnit()
-    local numLines = tooltip:NumLines()
-
+    
     if unit and UnitIsPlayer(unit) then
-		local localizedClass, englishClass = UnitClass(unit)
+		local className, classFilename = UnitClass(unit)
+        if not className or not classFilename then
+            return
+        end
+        local numLines = tooltip:NumLines()
         for i = 2, numLines do
 			local line = _G["GameTooltipTextLeft" .. i]
 			if line then
 				local lineText = line:GetText()
-				if lineText and lineText:find(localizedClass) then
-					local classColor = RAID_CLASS_COLORS[englishClass]
+				if lineText and lineText:find(className) then
+					local classColor = RAID_CLASS_COLORS[classFilename]
                     if classColor then
                         line:SetFormattedText(lineText:gsub(lineText, CLASS_FORMAT), classColor.colorStr, lineText)
                     end
